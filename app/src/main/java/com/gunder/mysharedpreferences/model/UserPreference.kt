37aac55e46cc85272e8b11675ec406b16268141c
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-17 21:24:07 +0700

    mnmbhkan class UserPreference.kt dan kodenya

---
+++ app/src/main/java/com/gunder/mysharedpreferences/model/UserPreference.kt
@@ -0,0 +1,14 @@
+package com.gunder.mysharedpreferences.model
+
+import android.content.Context
+
+internal class UserPreference(context: Context) {
+    companion object {
+        private const val PREFS_NAME = "user_pref"
+        private const val NAME = "name"
+        private const val EMAIL = "email"
+        private const val AGE = "age"
+        private const val PHONE_NUMBER = "phone"
+        private const val LOVE_MU = "isLove"
+    }
+}
